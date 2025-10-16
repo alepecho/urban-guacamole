5908a2b328a4d191e06edfced1306a07a28dd756
alepecho <alepecho@gmail.com>
2025-10-16 10:09:03 -0600

    CAMBIOS PARA REALIZAR PRUEBA EN GITHUB Y LOCAL

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,7 +2,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| Create The Application
+| Create The Application CAMBIOS PARA REALIZAR PRUEBA EN GITHUB Y LOCAL
 |--------------------------------------------------------------------------
 |
 | The first thing we will do is create a new Laravel application instance
